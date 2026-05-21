f162ef57c132a8896e4f1e7ae18e5909ebef2bac
jasmit.bajariya <jasmit.bajariya@unlocktech.co.in>
2026-05-21 11:58:07 +0530

    structure changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,64 @@
+// import js from '@eslint/js'
+// import globals from 'globals'
+// import reactHooks from 'eslint-plugin-react-hooks'
+// import reactRefresh from 'eslint-plugin-react-refresh'
+// import { defineConfig, globalIgnores } from 'eslint/config'
+
+// export default defineConfig([
+//   globalIgnores(['dist']),
+//   {
+//     files: ['**/*.{js,jsx}'],
+//     extends: [
+//       js.configs.recommended,
+//       reactHooks.configs.flat.recommended,
+//       reactRefresh.configs.vite,
+//     ],
+//     languageOptions: {
+//       globals: globals.browser,
+//       parserOptions: { ecmaFeatures: { jsx: true } },
+//     },
+//   },
+// ])
+
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      // 🔕 Ignore ALL requested errors
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-debugger': 'off',
+
+
+      // ⚠️ Enable warnings for undefined variables
+      'no-undef': 'warn',
+      '@typescript-eslint/no-undef': 'warn',
+      
+
+      // React rules
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
-])
+)
